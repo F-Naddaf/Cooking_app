fe05b40a9372ccf590970fe1a1aacd12eb0bb3bf
Fadi <fady-naddaf@hotmail.com>
2022-06-06 16:00:58 +0200

    changes of the recipe

--- src/Pages/recipe-page.js
+++ src/Pages/recipe-page.js
@@ -1,14 +1,15 @@
 'use strict';
 import { mealRecipeModal } from '../views/recipe-view.js';
-// import { createMeal } from '../views/init-view.js';
 
 export async function getMealRecipe() {
-      let mealItem = mealRecipeModal(meal);
-      try {
-        await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-        const response = await response.json();
-        const data = mealRecipeModal(data.meal)
-      } catch (errors) {
-        console.log(errors);
-    }
+  let mealItem = mealRecipeModal(meal);
+  try {
+    await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`,
+    );
+    const response = await response.json();
+    const data = mealRecipeModal(response.meal);
+  } catch (errors) {
+    console.log(errors);
   }
+}
